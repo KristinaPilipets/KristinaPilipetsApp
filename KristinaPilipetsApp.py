b534a35b158ccc09cd80db7a1a7e1fa15e94ee16
opilane <opilane@HP-CZC6297BB1>
2021-10-29 10:48:41 +0300

    .-.

--- KristinaPilipetsApp.py
+++ KristinaPilipetsApp.py
@@ -126,3 +126,24 @@ if choice==1:
 	print(f"компьютер выиграл {wina} раза, вы выиграли {winans} раза, ничья {f}")
 elif choice==2:
 	print(f"игрок 1 выиграл {wina} раза, игрок 2 выиграл {winans} раза")
+print()
+print("--------------------------")
+print()
+v1=["Kivi","Käärid","Paber"]
+v2=["Kivi","Käärid","Paber"]
+if m==1:
+	while True:
+		print("kas mängime? esc - välja, enter - mängimine")
+		if read_key()=="esc":
+			break
+		elif read_key()=="enter":
+			p1=choice(v1)
+			print("Esimine bot: ",p1)
+			p2=choice(v2)
+			print("Teine bot: ",p2)
+			if p1==p2:
+				print("Viik")
+			elif p1==v1[0] and p2==v2[1] or p1==v2[0] or p1==v1[1] and p2==v2[2]:
+				print("Võitis 1")
+			else:
+				print("Võitis 2")
