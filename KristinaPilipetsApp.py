b0bfe5c2425a731b677f134935e0aff19561307f
opilane <opilane@HP-CZC6297BB1>
2021-10-29 10:12:14 +0300

    .

--- KristinaPilipetsApp.py
+++ KristinaPilipetsApp.py
@@ -1,47 +1,121 @@
 from random import*
+from keyboard import*
 print("Камень, ножницы, бумага")
-print(" Камень >> 1","\n","Ножницы >> 2","\n","Бумага >> 3")
-ans=""
-while ans not in [1,2,3]:
+print(" Камень >> r","\n","Ножницы >> s","\n","Бумага >> p")
+fair=0
+wina=0
+winans=0
+b=""
+c=""
+print("Вы хотите играть с компьютером или с другим человеком?","\n","компьютер - 1","\n","человек - 2")
+choice=""
+while True:
 	try:
-		ans=int(input("Выберите камень, ножницы или бумага"))
+		print("Выберите вариант игры нажав клавишу 1 или 2 ")
+		if read_key()=="1":
+			choice=1
+			break
+		elif read_key()=="2":
+			choice=2
+			break
 	except:
-		TypeError
-a=randint(1,3)
-if a==1 and ans==1:
-	win="ничья"
-elif a==2 and ans==2:
-	win="ничья"
-elif a==3 and ans==3:
-	win="ничья"
-elif a==1 and ans==2:
-	win="Вы проиграли"
-elif a==1 and ans==3:
-	win="Вы победили"
-elif a==2 and ans==1:
-	win="Вы победили"
-elif a==2 and ans==3:
-	win="Вы проиграли"
-elif a==3 and ans==1:
-	win="Вы проиграли"
-elif a==3 and ans==2:
-	win="Вы победили"
-else:
-	win="Нет такого варианта"
+		ValueError
 
-if a==1:
-	b="камень"
-elif a==2:
-	b="ножницы"
-elif a==3:
-	b="бумага"
+print("Если вы хотите закончить игру нажмите q")
 
-if ans==1:
-	c="камень"
-elif ans==2:
-	c="ножницы"
-elif ans==3:
-	c="бумага"
+while read_key()!="q":
+	while True:
+		try:
+			print("Игрок 1. Выберите камень, ножницы или бумага")
+			if read_key()=="r":
+				ans=1
+				break
+			elif read_key()=="s":
+				ans=2
+				break
+			elif read_key()=="p":
+				ans=3
+				break
+		except:
+			ValueError
+	if choice==1:
+		a=randint(1,3)
+	elif choice ==2:
+		while True:
+			try:
+				print("Игрок 2. Выберите камень, ножницы или бумага")
+				if read_key()=="r":
+					a=1
+					break
+				elif read_key()=="s":
+					a=2
+					break
+				elif read_key()=="p":
+					a=3
+					break
+			except:
+				ValueError
+	
+	if int(a)==1 and int(ans)==1:
+		win="ничья"
+		fair+=1
+		f=0+fair
+	elif int(a)==2 and int(ans)==2:
+		win="ничья"
+		fair+=1
+		f=0+fair
+	elif int(a)==3 and int(ans)==3:
+		win="ничья"
+		fair+=1
+		f=0+fair
+	elif int(a)==1 and int(ans)==2:
+		win="Вы проиграли"
+		wina+=1
+		wa=0+wina
+	elif int(a)==1 and int(ans)==3:
+		win="Вы победили"
+		winans+=1
+		wans=0+winans
+	elif int(a)==2 and int(ans)==1:
+		win="Вы победили"
+		winans+=1
+		wans=0+winans
+	elif int(a)==2 and int(ans)==3:
+		win="Вы проиграли"
+		wina+=1
+		wa=0+wina
+	elif int(a)==3 and int(ans)==1:
+		win="Вы проиграли"
+		wina+=1
+		wa=0+wina
+	elif int(a)==3 and int(ans)==2:
+		win="Вы победили"
+		winans+=1
+		wans=0+winans
+	else:
+		print("нет такого варианта")
+	
+	if int(a)==1:
+		b="камень"
+	elif int(a)==2:
+		b="ножницы"
+	elif int(a)==3:
+		b="бумага"
 
-print(f"компьютер выбрал {b}, вы выбрали {c}")
-print(win)
+	if int(ans)==1:
+		c="камень"
+	elif int(ans)==2:
+		c="ножницы"
+	elif int(ans)==3:
+		c="бумага"
+	print()
+	if choice==1:
+		print(f"компьютер выбрал {b}, вы выбрали {c}")
+	elif choice==2:
+		print(f"игрок 1 выбрал {b}, игрок 2 выбрал {c}")
+
+print("игра окончена")
+if choice==1:
+	print(f"компьютер выиграл {wina} раза, вы выиграли {winans} раза")
+elif choice==2:
+	print(f"игрок 1 выиграл {wina} раза, игрок 2 выиграл {winans} раза")
