9ab3b499515454142dcb8b3cfbc6ba3b2fb03353
opilane <opilane@HP-CZC6297BB1>
2021-11-10 11:18:18 +0200

    .

--- workDEF/tsukel.py
+++ workDEF/tsukel.py
@@ -1,1 +1,12 @@
-
+def arithmetic(a: float,b:float,c:str):
+	"""lihtne kalkulaator
+	+ - liitmine
+	- - lahutamine
+	* - korrutamine
+	/ - jagamine
+	:param float a: esimine arv
+	:param float b: Teine arv
+	:param str c: aritmeetiline tehing
+	:rtype float:
+	"""
+	
